db3b8c5c86efa4ef9f323da58bb416946727bad2
Denff <manowardenff@gmail.com>
2021-08-04 22:15:21 +0500

    consts

--- src/js/components/counterSkills.js
+++ src/js/components/counterSkills.js
@@ -5,101 +5,101 @@ if (document.querySelector('.anketa__js-counter')) {
     const blueColor = '#0093d7';
     const arrowNeedle =  document.querySelector('.anketa__js-arrow');
     const approvedImg = document.querySelector('.anketa__approved-img');
-    const ch = document.querySelectorAll('.checkbox');
+    const checkboxs = document.querySelectorAll('.checkbox');
     const counterOut = document.querySelector('.anketa__js-counter');
-    let arr_1 = [];
-    let sumOut = 0;
+    let skills = [];
+    let sum = 0;
 
-    const sumArrElements = (arr) => {
+    const sumSkills = (arr) => {
         let result = 0;
-        for (i = 0; i < arr.length; i++){
+        for (let i = 0; i < arr.length; i++){
             result += arr[i];
         }
         return result;
     }
 
-    const plusFunc = () => {
+    const plusSkill = () => {
 
-        for(let i in ch) {
-            ch[i].disabled = true;   
+        for(let i in checkboxs) {
+            checkboxs[i].disabled = true;   
             setTimeout(()=> {
-                ch[i].disabled = false;
+                checkboxs[i].disabled = false;
             }, 425);
         }
 
         let interval = setInterval(() => {
-            sumOut = sumOut + 1;
-            let s = sumArrElements(arr_1);
+            sum = sum + 1;
+            let sumSkillsStop = sumSkills(skills);
 
-            if (sumOut <= 320) {
+            if (sum <= 320) {
                 counterOut.style.color=yellowColor;
             }
-            else if (sumOut >= 321 && sumOut <= 670) {
+            else if (sum >= 321 && sum <= 670) {
                 counterOut.style.color=greenColor;
             }
-            else if (sumOut >= 671) {
+            else if (sum >= 671) {
                 counterOut.style.color=blueColor;
             }
 
-            if (sumOut === s) {
+            if (sum === sumSkillsStop) {
                 clearInterval(interval);
             }
 
-            arrowNeedle.style.transform = 'rotate(' + (285 + ((sumOut * 180) / 1008)) + 'deg)';
-            counterOut.innerHTML = sumOut;
+            arrowNeedle.style.transform = 'rotate(' + (285 + ((sum * 180) / 1008)) + 'deg)';
+            counterOut.innerHTML = sum;
         }, 5); 
 
     }
-    const minusFunc = () => {
+    const minusSkill = () => {
 
-        for(let i in ch) {
-            ch[i].disabled = true;
+        for(let i in checkboxs) {
+            checkboxs[i].disabled = true;
             setTimeout(()=> {
-                ch[i].disabled = false;
+                checkboxs[i].disabled = false;
             }, 425);
         }
 
-        if (sumOut > 0) {
-
-            let interval = setInterval(() => {
-                sumOut = sumOut - 1;
-    
-                let s;
-                arr_1.length ? s = sumArrElements(arr_1) : s = 0;
-    
-                if (sumOut <= 320) {
-                    counterOut.style.color=yellowColor;
-                }
-                else if (sumOut >= 321 && sumOut <= 670) {
-                    counterOut.style.color=greenColor;
-                }
-                else if (sumOut >= 671) {
-                    counterOut.style.color=blueColor;
-                }
-    
-                if (  sumOut === s ) {
-                    clearInterval(interval);
-                }
-    
-                arrowNeedle.style.transform = 'rotate(' + (285 + ((sumOut * 180) / 1008)) + 'deg)';
-                counterOut.innerHTML = sumOut;
-            }, 5); 
+        if (sum < 0) {
+            return;
         }
+
+        let interval = setInterval(() => {
+            sum = sum - 1;
+
+            let sumSkillsStop = 0 < skills.length ? sumSkills(skills) : 0;
+
+            if (sum <= 320) {
+                counterOut.style.color=yellowColor;
+            }
+            else if (sum >= 321 && sum <= 670) {
+                counterOut.style.color=greenColor;
+            }
+            else if (sum >= 671) {
+                counterOut.style.color=blueColor;
+            }
+
+            if (  sum === sumSkillsStop ) {
+                clearInterval(interval);
+            }
+
+            arrowNeedle.style.transform = 'rotate(' + (285 + ((sum * 180) / 1008)) + 'deg)';
+            counterOut.innerHTML = sum;
+        }, 5); 
     }
 
-    const checkFunc = (elem) => {
+    const checkSkill = (elem) => {
         
         if(elem.target.checked) {
 
-            arr_1[arr_1.length] = 84;
-            plusFunc();
+            skills[skills.length] = 84;
+            plusSkill();
 
         } else {
-            arr_1.pop();
-            minusFunc();
+            skills.pop();
+            minusSkill();
         }
         
-        if (arr_1.length > 6) {
+        if (skills.length > 5) {
             approvedImg.classList.add('active');
         } else {
             if(approvedImg.classList.contains('active')) {
@@ -108,9 +108,7 @@ if (document.querySelector('.anketa__js-counter')) {
         }
     }
 
-    const changeCheckState = ch.forEach((item) => {
-        item.addEventListener('change', (e) => {
-            checkFunc(e);
-        });
+    const changeCheckState = checkboxs.forEach((item) => {
+        item.addEventListener('change', checkSkill);
     });
 }
