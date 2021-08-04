7a1cb0d539ba8afe6ad6f6a8c0b4f03151780498
Denff <manowardenff@gmail.com>
2021-08-04 18:50:40 +0500

    fix3

--- src/js/components/counterSkills.js
+++ src/js/components/counterSkills.js
@@ -1,17 +1,22 @@
 if (document.querySelector('.anketa__js-counter')) {
-  
     
     const yellowColor = '#ffc815';
     const greenColor = '#a3cd3b';
     const blueColor = '#0093d7';
     const arrowNeedle =  document.querySelector('.anketa__js-arrow');
-
-
+    const approvedImg = document.querySelector('.anketa__approved-img');
     const ch = document.querySelectorAll('.checkbox');
-    const counterOut = document.querySelector('.anketa__js-counter')
+    const counterOut = document.querySelector('.anketa__js-counter');
     let arr_1 = [];
     let sumOut = 0;
 
+    const sumArrElements = (arr) => {
+        let result = 0;
+        for (i = 0; i < arr.length; i++){
+            result += arr[i];
+        }
+        return result;
+    }
 
     const plusFunc = () => {
 
@@ -19,44 +24,68 @@ if (document.querySelector('.anketa__js-counter')) {
             ch[i].disabled = true;   
             setTimeout(()=> {
                 ch[i].disabled = false;
-            }, 420);
+            }, 425);
         }
 
         let interval = setInterval(() => {
             sumOut = sumOut + 1;
+            let s = sumArrElements(arr_1);
 
-            let s = arr_1.reduce((a, b) => a+b);
+            if (sumOut <= 320) {
+                counterOut.style.color=yellowColor;
+            }
+            else if (sumOut >= 321 && sumOut <= 670) {
+                counterOut.style.color=greenColor;
+            }
+            else if (sumOut >= 671) {
+                counterOut.style.color=blueColor;
+            }
 
             if (sumOut === s) {
                 clearInterval(interval);
             }
+
+            arrowNeedle.style.transform = 'rotate(' + (285 + ((sumOut * 180) / 1008)) + 'deg)';
             counterOut.innerHTML = sumOut;
         }, 5); 
 
     }
     const minusFunc = () => {
 
-            for(let i in ch) {
-                ch[i].disabled = true;
-                setTimeout(()=> {
-                    ch[i].disabled = false;
-                }, 420);
-            }
+        for(let i in ch) {
+            ch[i].disabled = true;
+            setTimeout(()=> {
+                ch[i].disabled = false;
+            }, 425);
+        }
+
+        if (sumOut > 0) {
 
             let interval = setInterval(() => {
                 sumOut = sumOut - 1;
-
+    
                 let s;
-                arr_1.length ? s = arr_1.reduce((a, b) => a+b) : s = 0;
-
+                arr_1.length ? s = sumArrElements(arr_1) : s = 0;
+    
+                if (sumOut <= 320) {
+                    counterOut.style.color=yellowColor;
+                }
+                else if (sumOut >= 321 && sumOut <= 670) {
+                    counterOut.style.color=greenColor;
+                }
+                else if (sumOut >= 671) {
+                    counterOut.style.color=blueColor;
+                }
+    
                 if (  sumOut === s ) {
                     clearInterval(interval);
                 }
-
+    
+                arrowNeedle.style.transform = 'rotate(' + (285 + ((sumOut * 180) / 1008)) + 'deg)';
                 counterOut.innerHTML = sumOut;
             }, 5); 
+        }
     }
-
 
     const checkFunc = (elem) => {
         
@@ -69,35 +98,19 @@ if (document.querySelector('.anketa__js-counter')) {
             arr_1.pop();
             minusFunc();
         }
-    
-        return arr_1;
+        
+        if (arr_1.length > 6) {
+            approvedImg.classList.add('active');
+        } else {
+            if(approvedImg.classList.contains('active')) {
+                approvedImg.classList.remove('active');
+            }
+        }
     }
 
     const changeCheckState = ch.forEach((item) => {
         item.addEventListener('change', (e) => {
-
             checkFunc(e);
-
         });
     });
-
-
-
-
 }
-// arrowNeedle.style.transform = 'rotate(' + (285 + ((i * 180) / 1000)) + 'deg)';
-            // if (i <= 320) {
-            //     counterDiv.style.color=yellowColor;
-            // }
-            // else if (i >= 321 && i <= 670) {
-            //     counterDiv.style.color=greenColor;
-            // }
-            // else if (i >= 671) {
-            //     counterDiv.style.color=blueColor;
-            // }
-
-            // if (i === number) {
-            //     clearInterval(interval);
-            // }
-
-
--- src/js/components/form.js
+++ src/js/components/form.js
@@ -1,69 +1,17 @@
-
 if (document.querySelector('.field-input')) {
 	const input = document.querySelectorAll('.field-input');
-	const approvedImg = document.querySelector('.anketa__approved-img');
-
 
 	input.forEach((item) => {
 		item.addEventListener('focus', function() {
 			this.classList.add('active');
 		});
+
         item.addEventListener('blur', function() {
-  
             if (this.value == "") {
                 this.classList.remove('active');
             }
-
 		});
-
 	});
-	const jsCounter = document.querySelector('.anketa__js-counter');
-	// const IsApproveImg = () => {
-		if ( !approvedImg.classList.contains('active') && jsCounter.value > 400) {
-			approvedImg.classList.add('active');
-		}
-	// }
-
-
-	// setTimeout(IsApproveImg, 2000);
-
-
-
-// нужно добавлять класс active к этой картинке, когда все поля заполнены.
-// ('.anketa__approved-img')
-
-// это поля в верстке
-// .field
-// 	input.field-input(type="text" autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false" required)
-// 	label.field-label имя
-// .field
-// 	input.field-input(type="text" autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false" required)
-// 	label.field-label дата рождения
-// .field
-// 	input.field-input(type="text" autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false" required)
-// 	label.field-label город
-// .field
-// 	input.field-input(type="text" autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false" required)
-// 	label.field-label контактные данные
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
 
 
